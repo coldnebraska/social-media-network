f23af560b31057ac8946feb267abca97dddac11f
DBrush <coldnebraska@gmail.com>
2024-02-03 16:49:08 -0600

    added email validation and unique constraint

--- models/User.js
+++ models/User.js
@@ -4,11 +4,19 @@ const userSchema = new Schema(
   {
     username: {
       type: String,
+      trim: true,
       required: true,
     },
     email: {
       type: String,
       required: true,
+      unique: true,
+      validate: {
+        validator: function(email) {
+          return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(email)
+        },
+        message: email => `${email.value} is not a valid email`
+      }
     },
     thoughts: [],
     friends: [],
